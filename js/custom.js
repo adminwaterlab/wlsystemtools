990c67adc3bd17daa47bc0259fc101a12b736d60
sunairphil <sunaircargophil@gmail.com>
2022-08-24 14:52:58 +0800

    edit

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,3 @@
-self.addEventListener("load", function(){
-    if (localStorage.getItem("userid") == null) {
-  window.location = 'login.html';
-}
-  });
-$("#fullname").html(localStorage.getItem("fname") + ' ' + localStorage.getItem("lname"));
-$("#fname").html("Hi, " + localStorage.getItem("fname"));
 if ("serviceWorker" in navigator) {
   // register service worker
   navigator.serviceWorker.register("./serviceworker.js");
